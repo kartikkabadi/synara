Emanuele-web04/dpcode

feat(orchestration): persist goal in projection + green e2e loop
Make the goal feature work end to end through Synara's SQLite projection
(the reactor and web read the projection, not the in-memory model):
- migration 038 + projection_thread_goal table + repository
- threadGoal projector: persists goal lifecycle, turn/usage accounting,
  and continuation count (shares goalProjection.ts helpers with the
  in-memory projector — no duplicated logic)
- ProjectionSnapshotQuery hydrates thread.goal across all read paths
- harness starts the GoalContinuationReactor; integration e2e drives a
  full continue-until-sentinel loop and passes
- reactor hardened: require the completed turn's assistant message before
  deciding, fixing a race that suppressed the goal before the sentinel
  message was persisted
- update projector / snapshot expectations for the new goal field

Co-Authored-By: Claude Opus 4.8 <noreply@anthropic.com>

apps/server/src/orchestration/projector.ts
@@ -1,9 +1,4 @@
-import type {
-  OrchestrationEvent,
-  OrchestrationGoal,
-  OrchestrationReadModel,
-  ThreadId,
-} from "@t3tools/contracts";
+import type { OrchestrationEvent, OrchestrationReadModel, ThreadId } from "@t3tools/contracts";
 import {
   OrchestrationCheckpointSummary,
   OrchestrationMessage,
@@ -35,6 +30,11 @@ import {
   ThreadTurnDiffCompletedPayload,
   ThreadTurnStartRequestedPayload,
 } from "./Schemas.ts";
+import {
+  applyGoalTurnAccounting,
+  incrementGoalContinuation,
+  transitionGoalStatus,
+} from "./goalProjection.ts";
 
 type ThreadPatch = Partial<Omit<OrchestrationThread, "id" | "projectId">>;
 const MAX_THREAD_MESSAGES = 2_000;
@@ -66,78 +66,6 @@ function updateThread(
   patch: ThreadPatch,
 ): OrchestrationThread[] {
   return threads.map((thread) => (thread.id === threadId ? { ...thread, ...patch } : thread));
-}
-
-function goalElapsedSeconds(createdAt: string, completedAt: string): number {
-  const start = Date.parse(createdAt);
-  const end = Date.parse(completedAt);
-  if (!Number.isFinite(start) || !Number.isFinite(end) || end <= start) {
-    return 0;
-  }
-  return Math.floor((end - start) / 1000);
-}
-
-// Best-effort per-turn token usage extraction from a provider-shaped (Json) activity
-// payload. Mirrors pi-goal's tolerant `extractUsageAccounting`: providers report usage
-// under varying key names, so we probe a set of aliases and fall back to zero.
-function extractTurnUsageFromActivityPayload(payload: unknown): {
-  inputTokens: number;
-  outputTokens: number;
-  totalTokens: number;
-} {
-  if (typeof payload !== "object" || payload === null) {
-    return { inputTokens: 0, outputTokens: 0, totalTokens: 0 };
-  }
-  const nested = (payload as { usage?: unknown }).usage;
-  const source = (
-    typeof nested === "object" && nested !== null ? nested : payload
-  ) as Record<string, unknown>;
-  const num = (...keys: ReadonlyArray<string>): number => {
-    for (const key of keys) {
-      const value = source[key];
-      if (typeof value === "number" && Number.isFinite(value) && value >= 0) {
-        return Math.floor(value);
-      }
-    }
-    return 0;
-  };
-  const inputTokens = num("inputTokens", "input", "promptTokens", "input_tokens", "prompt_tokens");
-  const outputTokens = num(
-    "outputTokens",
-    "output",
-    "completionTokens",
-    "output_tokens",
-    "completion_tokens",
-  );
-  const explicitTotal = num("totalTokens", "total", "total_tokens");
-  const totalTokens = explicitTotal > 0 ? explicitTotal : inputTokens + outputTokens;
-  return { inputTokens, outputTokens, totalTokens };
-}
-
-// Fold a completed turn into the active goal: increment turn count, accumulate usage,
-// refresh elapsed time, and trip the budget guard. Mirrors pi-goal's `turn_end` handler.
-function applyGoalTurnAccounting(
-  goal: OrchestrationGoal,
-  activityPayload: unknown,
-  occurredAt: string,
-): OrchestrationGoal {
-  const delta = extractTurnUsageFromActivityPayload(activityPayload);
-  const tokensUsed = goal.tokensUsed + delta.totalTokens;
-  const usage = {
-    inputTokens: goal.usage.inputTokens + delta.inputTokens,
-    outputTokens: goal.usage.outputTokens + delta.outputTokens,
-    totalTokens: goal.usage.totalTokens + delta.totalTokens,
-  };
-  const budgetExhausted = goal.tokenBudget !== null && tokensUsed >= goal.tokenBudget;
-  return {
-    ...goal,
-    status: budgetExhausted ? "budget_limited" : goal.status,
-    tokensUsed,
-    usage,
-    turnCount: goal.turnCount + 1,
-    timeUsedSeconds: goalElapsedSeconds(goal.createdAt, occurredAt),
-    updatedAt: occurredAt,
-  };
 }
 
 function decodeForEvent<A>(
@@ -627,13 +555,7 @@ export function projectEvent(
         const goal = thread.goal;
         const goalContinuationPatch =
           goal && goal.status === "active" && payload.source === "goal-continuation"
-            ? {
-                goal: {
-                  ...goal,
-                  continuationCount: goal.continuationCount + 1,
-                  updatedAt: event.occurredAt,
-                },
-              }
+            ? { goal: incrementGoalContinuation(goal, event.occurredAt) }
             : {};
 
         return {
@@ -999,7 +921,7 @@ export function projectEvent(
           return {
             ...nextBase,
             threads: updateThread(nextBase.threads, payload.threadId, {
-              goal: { ...thread.goal, status: nextStatus, updatedAt: payload.updatedAt },
+              goal: transitionGoalStatus(thread.goal, nextStatus, payload.updatedAt),
               updatedAt: event.occurredAt,
             }),
           };

kartikkabadi/synara

apps/server/src/orchestration/projector.ts
@@ -1,9 +1,4 @@
-import type {
-  OrchestrationEvent,
-  OrchestrationGoal,
-  OrchestrationReadModel,
-  ThreadId,
-} from "@synara/contracts";
+import type { OrchestrationEvent, OrchestrationReadModel, ThreadId } from "@synara/contracts";
 import {
   OrchestrationCheckpointSummary,
   OrchestrationMessage,
@@ -56,6 +51,11 @@ import {
   ThreadTurnStartRequestedPayload,
 } from "./Schemas.ts";
 import { resolveStableMessageTurnId } from "./messageTurnId.ts";
+import {
+  applyGoalTurnAccounting,
+  incrementGoalContinuation,
+  transitionGoalStatus,
+} from "./goalProjection.ts";
 
 type ThreadPatch = Partial<Omit<OrchestrationThread, "id" | "projectId">>;
 const MAX_THREAD_MESSAGES = 2_000;
@@ -123,78 +123,6 @@ function updateThread(
   patch: ThreadPatch,
 ): OrchestrationThread[] {
   return threads.map((thread) => (thread.id === threadId ? { ...thread, ...patch } : thread));
-}
-
-function goalElapsedSeconds(createdAt: string, completedAt: string): number {
-  const start = Date.parse(createdAt);
-  const end = Date.parse(completedAt);
-  if (!Number.isFinite(start) || !Number.isFinite(end) || end <= start) {
-    return 0;
-  }
-  return Math.floor((end - start) / 1000);
-}
-
-// Best-effort per-turn token usage extraction from a provider-shaped (Json) activity
-// payload. Mirrors pi-goal's tolerant `extractUsageAccounting`: providers report usage
-// under varying key names, so we probe a set of aliases and fall back to zero.
-function extractTurnUsageFromActivityPayload(payload: unknown): {
-  inputTokens: number;
-  outputTokens: number;
-  totalTokens: number;
-} {
-  if (typeof payload !== "object" || payload === null) {
-    return { inputTokens: 0, outputTokens: 0, totalTokens: 0 };
-  }
-  const nested = (payload as { usage?: unknown }).usage;
-  const source = (
-    typeof nested === "object" && nested !== null ? nested : payload
-  ) as Record<string, unknown>;
-  const num = (...keys: ReadonlyArray<string>): number => {
-    for (const key of keys) {
-      const value = source[key];
-      if (typeof value === "number" && Number.isFinite(value) && value >= 0) {
-        return Math.floor(value);
-      }
-    }
-    return 0;
-  };
-  const inputTokens = num("inputTokens", "input", "promptTokens", "input_tokens", "prompt_tokens");
-  const outputTokens = num(
-    "outputTokens",
-    "output",
-    "completionTokens",
-    "output_tokens",
-    "completion_tokens",
-  );
-  const explicitTotal = num("totalTokens", "total", "total_tokens");
-  const totalTokens = explicitTotal > 0 ? explicitTotal : inputTokens + outputTokens;
-  return { inputTokens, outputTokens, totalTokens };
-}
-
-// Fold a completed turn into the active goal: increment turn count, accumulate usage,
-// refresh elapsed time, and trip the budget guard. Mirrors pi-goal's `turn_end` handler.
-function applyGoalTurnAccounting(
-  goal: OrchestrationGoal,
-  activityPayload: unknown,
-  occurredAt: string,
-): OrchestrationGoal {
-  const delta = extractTurnUsageFromActivityPayload(activityPayload);
-  const tokensUsed = goal.tokensUsed + delta.totalTokens;
-  const usage = {
-    inputTokens: goal.usage.inputTokens + delta.inputTokens,
-    outputTokens: goal.usage.outputTokens + delta.outputTokens,
-    totalTokens: goal.usage.totalTokens + delta.totalTokens,
-  };
-  const budgetExhausted = goal.tokenBudget !== null && tokensUsed >= goal.tokenBudget;
-  return {
-    ...goal,
-    status: budgetExhausted ? "budget_limited" : goal.status,
-    tokensUsed,
-    usage,
-    turnCount: goal.turnCount + 1,
-    timeUsedSeconds: goalElapsedSeconds(goal.createdAt, occurredAt),
-    updatedAt: occurredAt,
-  };
 }
 
 function decodeForEvent<A>(
@@ -884,13 +812,7 @@ export function projectEvent(
         const goal = thread.goal;
         const goalContinuationPatch =
           goal && goal.status === "active" && payload.source === "goal-continuation"
-            ? {
-                goal: {
-                  ...goal,
-                  continuationCount: goal.continuationCount + 1,
-                  updatedAt: event.occurredAt,
-                },
-              }
+            ? { goal: incrementGoalContinuation(goal, event.occurredAt) }
             : {};
 
         return {
@@ -1262,7 +1184,7 @@ export function projectEvent(
           return {
             ...nextBase,
             threads: updateThread(nextBase.threads, payload.threadId, {
-              goal: { ...thread.goal, status: nextStatus, updatedAt: payload.updatedAt },
+              goal: transitionGoalStatus(thread.goal, nextStatus, payload.updatedAt),
               updatedAt: event.occurredAt,
             }),
           };